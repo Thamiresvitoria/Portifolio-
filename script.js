f9afe046bb01ab514c88470bb28085501dc148e4
Thamiresvitoria <thamiresvitoriamunizdasilva20@gmail.com>
2026-08-07 17:09:01 -0300

    testando a barrinha

--- script.js
+++ script.js
@@ -1,43 +1,13 @@
-// Pegue todas as sections
-const sections = document.querySelectorAll("section");
-
-// Pegue todos os links da navbar
 const links = document.querySelectorAll("nav a");
 
-// Crie um observador
-const observer = new IntersectionObserver((entries) => {
+links.forEach(link => {
+    link.addEventListener("click", () => {
 
-    // Para cada section que o observador detectar
-    entries.forEach(entry => {
+        links.forEach(item => {
+            item.classList.remove("active");
+        });
 
-        // Se a section estiver visível
-        if (entry.isIntersecting) {
+        link.classList.add("active");
 
-            // Remova a barrinha de todos os links
-            links.forEach(link => {
-                link.classList.remove("active");
-            });
-
-            // Encontre o link que corresponde à section
-            const linkAtivo = document.querySelector(
-                `nav a[href="#${entry.target.id}"]`
-            );
-
-            // Se encontrou o link
-            if (linkAtivo) {
-
-                // Coloque a barrinha nele
-                linkAtivo.classList.add("active");
-            }
-        }
     });
-
-}, {
-    // Considere ativo quando 50% estiver visível
-    threshold: 0.5
-});
-
-// Observe todas as sections
-sections.forEach(section => {
-    observer.observe(section);
 });
